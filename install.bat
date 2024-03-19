cbb9567fee7a0a52f6007ad09f1f632825e0108b
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-03-19 20:53:19 +0100

    updated install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-copy "C:\Users\Fynn Haupt\CodeProjects\krp-sharedmemory-plugin\Debug\krp-sharedmemory-plugin.dll" "G:\SteamLibrary\steamapps\common\Kart Racing Pro\plugins\krp-sharedmemory-plugin.dlo"
+copy "C:\Users\Fynn Haupt\CodeProjects\krp-sharedmemory-plugin\build\Release\krp-sharedmemory-plugin.dll" "G:\SteamLibrary\steamapps\common\Kart Racing Pro\plugins\krp-sharedmemory-plugin.dlo"
